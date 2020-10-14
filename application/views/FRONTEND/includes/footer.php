e03e6c97a0c744da13fec70cf105929760d66bb8
shankargouda <skarigouda87@gmail.com>
2020-10-15 02:56:37 +0530

    footer problem for addcart

--- application/views/FRONTEND/includes/footer.php
+++ application/views/FRONTEND/includes/footer.php
@@ -1,8 +1,4 @@
 <!--? Services Area Start -->
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 <div class="categories-area">
     <div class="container-fluid">
         <div class="row" style="padding-top: 61px;">
@@ -257,11 +253,9 @@ function wishlist(prdid) {
             if (tempdata.trim() != "") {
                 var values = JSON.parse(tempdata);
                 if (values.success == "1") {
-                    window.location.href = "<?php echo base_url("
-                    WishList "); ?>";
+                    window.location.href = "<?php echo base_url("WishList "); ?>";
                 } else {
-                    window.location.href = "<?php echo base_url("
-                    Login "); ?>";
+                    window.location.href = "<?php echo base_url("Login "); ?>";
                 }
             } else {
                 $(".addtocartres" + prdid).html("Something went wrong, Please try again later.");
@@ -273,6 +267,7 @@ function wishlist(prdid) {
 function addtocart(prdid) {
     var prdqty = $("#quantity" + prdid).val();
     var price = $("#pricevalue" + prdid).val();
+    console.log('kkk')
     var totalprice = Number(price * prdqty);
     $.ajax({
         type: "POST",
@@ -290,11 +285,9 @@ function addtocart(prdid) {
             if (tempdata.trim() != "") {
                 var values = JSON.parse(tempdata);
                 if (values.success == "1") {
-                    window.location.href = "<?php echo base_url("
-                    cart "); ?>";
+                    window.location.href = "<?php echo base_url("cart "); ?>";
                 } else {
-                    window.location.href = "<?php echo base_url("
-                    Login "); ?>";
+                    window.location.href = "<?php echo base_url("Login "); ?>";
                 }
             } else {
                 alert();
